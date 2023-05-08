6f8d534e89fc47cc13d43112e9e61513f2337e32
Gicherujoh <johnwambugu236@gmail.com>
2023-05-08 18:02:17 +0300

    Finshed adding some Changes on the Input fields and successfully implemented Login Functionality

--- src/Signup/WelcomePage.js
+++ src/Signup/WelcomePage.js
@@ -1,29 +1,87 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {yupResolver} from '@hookform/resolvers/yup';
 import {TextField,Button} from '@mui/material';
-import {NavLink} from 'react-router-dom'
+import {NavLink,useNavigate} from 'react-router-dom'
+import * as yup from 'yup';
+import {useForm} from 'react-hook-form'
 import './WelcomePage.css'
+import { db } from '../firebase';
+import { addDoc,collection,getDocs,docs } from 'firebase/firestore';
+
+const schema = yup.object().shape({
+  email:yup.string().email().required(),
+  password:yup.string().required().min(4).max(15)
+})
 const WelcomePage = () => {
+  const [data,setData] =useState([]);
+  const[password,setPassword]=useState('');
+  const[email,setEmail]= useState('');
+    const navigate = useNavigate();
+     const Navigate = ()=>{
+          navigate('/createaccount');
+     }
+    const {register,handleSubmit,formState:{errors}} = useForm({
+      resolver:yupResolver(schema)
+    })
+    const userCollection = collection(db,"users");
+      useEffect(()=>{
+        const ReadData= async()=>{
+          try{
+            const data = await getDocs(userCollection);
+            const filteredData = data.docs.map((doc)=>({
+              ...doc.data(),
+              id:doc.id
+            }))
+           setData(filteredData);
+          }catch(e){
+            console.error(e)
+          }
+
+        }
+        ReadData();
+
+      },[])
+    const Submit = async()=>{
+      data.forEach((item)=>{
+        if(item.Email === email && item.ConfirmPassword === password)
+        {
+          navigate('createaccount')
+        }
+
+      })
+
+    }
+  
+   
   return (
     <div className='welcome'>
         <img className='welcome__image' src='https://st4.depositphotos.com/1316534/41903/i/600/depositphotos_419033018-stock-photo-wroclaw-poland-sep-2020-man.jpg'/>
         <div className='inputs'>
-            <div className='input__fields'>
-             <TextField id="standard-basic" label="Email" variant="standard" className='input__field1'  />
+            <div className='inputs__field'>
+              <input placeholder='Email' name='email' className='input__field' {...register('email')} onChange={(e)=>setEmail(e.target.value)}/>
+              <p className='input__errors'>{errors.email && 'Enter a valid Email'}</p>
             </div>
-           
-           <TextField variant='standard' label='Password' className='input__field2' type='password'/>
-        
+            
+            <div>
+            <input placeholder='Password' name='password' type='password' className='input__field' {...register('password')} onChange={(e)=>setPassword(e.target.value)}/>
+            <p className='input__errors'>{errors.password && 'Invalid Password'}</p>
+            </div>
+
           <div className='login'>
-           <Button variant="contained" className='login__button'>Login</Button>
+           <Button variant="contained" className='login__button' onClick={handleSubmit(Submit)}>Login</Button>
+        </div>
+        <div className='forgotPassword'>
+            <NavLink>Forgot Password?</NavLink>
         </div>
            <div className='Border'>
 
            </div>
            <div className='create__account'>
-              <Button variant='contained' color='success' className='account__button'>Create Account</Button>
+              <Button variant='contained' color='success' className='account__button' onClick={Navigate}>Create Account</Button>
            </div>
      </div>
     </div>
+    
   )
 }
 
