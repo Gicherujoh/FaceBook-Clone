2ac5ebdd4c559e0761d8945841ddbce515c5225b
Gicherujoh <johnwambugu236@gmail.com>
2023-05-19 16:33:28 +0300

    Finshed implementing User Post Functionality

--- src/Main/Post.js
+++ src/Main/Post.js
@@ -1,14 +1,17 @@
 import React,{useEffect,useState} from 'react'
-import {getDocs,collection,docs} from 'firebase/firestore'
+import {getDocs,collection,docs,doc,updateDoc} from 'firebase/firestore'
 import {Person} from '@mui/icons-material'
 import {db} from '../firebase'
 import {Button} from '@mui/material'
 import {ArrowBack} from '@mui/icons-material'
+import {useNavigate} from 'react-router-dom'
 import './Post.css'
 const Post = () => {
     const userRef = collection(db,'users');
     const [users,setUsers] = useState([]);
     const [emailRef,setEmailRef] = useState('')
+    const  [Input,setInput] = useState('');
+    const [show,setShow] = useState(false)
     useEffect(()=>{
         const email = localStorage.getItem('email');
         setEmailRef(email);
@@ -36,12 +39,30 @@ const Post = () => {
     }
     const currentUser = users.find(FindUser);
     console.log(currentUser)
+    
+    
+    const navigate = useNavigate();
+    const Navigate = ()=>{
+         navigate('/')
+    }
+    const currentUserPost = doc(db,`users/${currentUser?.id}`);
+    const Post = async()=>{
+        try{
+              await updateDoc(currentUserPost,{
+                  UserPost:Input
+              })
+              navigate('/')
+        }catch(e){
+          console.error(e)
+        }
+
+    }
   return (
     <div>
           <div className='post__nav'>
               <div className='post__icon'>
                   <div>
-                      <ArrowBack className='post__arrowback'/>
+                      <ArrowBack className='post__arrowback' onClick={Navigate}/>
                   </div>
                   <div>
                       <h2 className='post__title'>Create Post</h2>
@@ -49,7 +70,7 @@ const Post = () => {
 
               </div>
               <div>
-               <Button variant='contained' color='primary'>Post</Button>
+               <Button variant='contained' color='primary' onClick={Post}>Post</Button>
               </div>
           </div>
           <div className='Border'>
@@ -66,7 +87,7 @@ const Post = () => {
           </div>
 
           <div>
-              <input placeholder='What is in your Mind?' className='post__input'/>
+              <input placeholder='What is in your Mind?' className='post__input' onChange={(e)=>setInput(e.target.value)}/>
           </div>
       
     </div>
